97f6dc63b7a7908f9a0b1ece007e458043039466
hwangchunlee <elluard@me.com>
2021-08-03 22:54:00 +0900

    아이템 구현 기본틀 완료

--- src/main/scala/InstantTestSheet.sc
+++ src/main/scala/InstantTestSheet.sc
@@ -1,5 +1,2 @@
 import Items._
-
-val item1 = ItemRef(1, "사격형 파우더", "소모품", "스킬파우더", sellable = true)
-val item2 = ItemManager.createItem(2)
-val item2 = ItemManager.createItem(3)
+val item2 = ItemManager.createItem(1)
--- src/main/scala/Items/ItemRef.scala
+++ src/main/scala/Items/ItemRef.scala
@@ -2,11 +2,11 @@ package Items
 
 case class ItemRef(id: Int, name : String, category1: String, category2: String, sellable: Boolean)
 
+//TODO : 한번에 모든 아이템을 다 만들기는 쉽지 않다, 기본틀만 잡아두고, 다른 컨텐츠 구현 할 때, 필요한 아이템을 추가하도록 하자.
 object ItemManager {
   def createItem(id : Int): Option[ItemRef] =  {
     id match {
       case 1 => Some(ItemRef(1, "혼돈의 케이크 파편", "소모품", "혼돈의 케이크 파편",  sellable = true))
-      case 2 => Some(ItemRef(2, "경험의 별사탕 레벨1", "소모품", "경험의 별사탕",  sellable = true))
       case _ => None
     }
   }
