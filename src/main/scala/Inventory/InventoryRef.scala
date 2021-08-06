aa58342ab266bd2420fb23cd8b58319f8934063a
elluard <elluard@me.com>
2021-08-06 23:47:37 +0900

    인벤토리 아이템 추가/삭제 구성완료

--- src/main/scala/Inventory/InventoryRef.scala
+++ src/main/scala/Inventory/InventoryRef.scala
@@ -4,24 +4,50 @@ import Items._
 
 import scala.collection.mutable
 
-case class InvenUnit (itemId : Int, count : Int)
+case class InvenUnit(itemId : Int, count : Int, maxStack: Int)
 
-class InventoryRef(var inventory : mutable.IndexedSeq[InvenUnit]) {
+class InventoryRef(var inventory : mutable.Map[Int/*ItemID*/, InvenUnit]) {
+  var maxCount = 5
+
+  def usingCount : Int =
+    inventory.foldLeft(0){(acc, curr)=>
+      acc + ((curr._2.count - 1) / curr._2.maxStack + 1)
+    }
+
   def addItem(item : ItemRef) : Unit = {
-    val index = inventory.indexWhere{ inven =>
-      inven.itemId == item.id && inven.count < item.maxStack
-    }
+    val tempInven = inventory.clone()
+    val inven = tempInven.getOrElse(item.id, InvenUnit(item.id, 0, item.maxStack))
+    tempInven.update(item.id, InvenUnit(item.id, inven.count + 1, item.maxStack))
 
-    if(index == -1) {
-      this.inventory = this.inventory :+ InvenUnit(itemId = item.id, count = 1)
-    }
-    else {
-      val tempInven = inventory(index)
-      inventory.update(index, InvenUnit(tempInven.itemId, tempInven.count + 1))
+    //허용된 슬롯 갯수를 체크하기위한 count 계산
+    if(usingCount <= maxCount) {
+      inventory = tempInven
     }
   }
 
-  def addItem(invenUnit : InvenUnit) : Unit = {
-    this.inventory = this.inventory :+ invenUnit
+  def addItem(item : InvenUnit) : Unit = {
+    val tempInven = inventory.clone()
+    val inven = tempInven.getOrElse(item.itemId, InvenUnit(item.itemId, 0, item.maxStack))
+    tempInven.update(inven.itemId, InvenUnit(inven.itemId, inven.count + item.count, item.maxStack))
+
+    if(usingCount <= maxCount) {
+      inventory = tempInven
+    }
+  }
+
+  def delItem(invenUnit: InvenUnit) : Unit = {
+    val result = inventory.get(invenUnit.itemId) match {
+      case Some(item) if invenUnit.count <= item.count => Some(InvenUnit(item.itemId, item.count - invenUnit.count, item.maxStack))
+      case _ => None
+    }
+
+    result.foreach{ unit =>
+      if(unit.count < 1) {
+        inventory.remove(unit.itemId)
+      }
+      else {
+        inventory.update(unit.itemId, unit)
+      }
+    }
   }
 }
--- src/test/scala/Inventory/InventoryTest.scala
+++ src/test/scala/Inventory/InventoryTest.scala
@@ -7,33 +7,52 @@ import scala.collection.mutable
 
 class InventoryTest extends AnyFlatSpec {
   "인벤토리 매니저" should "새로운 아이템 추가 시, 요소 추가" in {
-    val inventory = new InventoryRef(mutable.IndexedSeq[InvenUnit]())
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
     inventory.addItem(ItemManager.createItem(1).get)
-    assert(inventory.inventory.length == 1)
+    assert(inventory.inventory.size == 1)
+    assert(inventory.inventory(1).count == 1)
     inventory.addItem(ItemManager.createItem(2).get)
-    assert(inventory.inventory.length == 2)
+    assert(inventory.inventory.size == 2)
+    assert(inventory.inventory(2).count == 1)
   }
 
   "인벤토리 매니저" should "기존 아이템 추가 시, 카운트 증가" in {
-    val inventory = new InventoryRef(mutable.IndexedSeq[InvenUnit]())
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
     inventory.addItem(ItemManager.createItem(1).get)
     inventory.addItem(ItemManager.createItem(1).get)
-    assert(inventory.inventory.length == 1)
-    assert(inventory.inventory(0).count == 2)
+    assert(inventory.inventory.size == 1)
+    assert(inventory.inventory(1).count == 2)
   }
 
-  "인벤토리 매니저" should "기존 아이템 max stack 값 초과시, 새로 추가" in {
-    val inventory = new InventoryRef(mutable.IndexedSeq[InvenUnit]())
-    for(x <- 0 to 998) {
-      inventory.addItem(ItemManager.createItem(1).get)
-    }
+  "인벤토리 매니저" should "InvenUnit 으로 아이템 추가 가능 " in {
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
+    inventory.addItem(InvenUnit(1, 10, 10))
+    assert(inventory.inventory.size == 1)
+    assert(inventory.inventory(1).count == 10)
+  }
 
-    assert(inventory.inventory.length === 1)
-    assert(inventory.inventory(0).count === 999)
+  "인벤토리 매니저" should "인벤토리 max slot 체크" in {
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
+    inventory.addItem(InvenUnit(1, 10, 2))
+    inventory.addItem(InvenUnit(2, 10, 3))
+    inventory.addItem(InvenUnit(3, 10, 2))
 
-    inventory.addItem(ItemManager.createItem(1).get)
+    assert(inventory.inventory.size == 2)
+  }
 
-    assert(inventory.inventory.length === 2)
-    assert(inventory.inventory(1).count === 1)
+  "인벤토리 매니저" should "인벤토리 삭제 체크" in {
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
+    inventory.addItem(InvenUnit(1, 10, 2))
+    inventory.delItem(InvenUnit(1, 5, 2))
+
+    assert(inventory.inventory(1).count == 5)
+  }
+
+  "인벤토리 매니저" should "전량 삭제 시, 슬롯에서 빠짐 체크 " in {
+    val inventory = new InventoryRef(mutable.Map[Int, InvenUnit]())
+    inventory.addItem(InvenUnit(1, 10, 2))
+    inventory.delItem(InvenUnit(1, 10, 2))
+
+    assert(inventory.inventory.isEmpty)
   }
 }
